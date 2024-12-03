11bcb3773826e91470f34e0eb48d1f71af73e58c
StevenMoyes <steven.moyes@protonmail.com>
2024-12-03 12:42:02 +0000

    splash screen added

---
+++ src/Components/SplashScreen.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+
+const SplashScreen = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const handleClick = () => setIsVisible(false);
+    document.addEventListener("click", handleClick);
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  return (
+    isVisible && (
+      <div className='open-splash'>
+        <img src='./mortgage_ad_design_page.png' alt='Splash Screen' />
+      </div>
+    )
+  );
+};
+
+export default SplashScreen;
